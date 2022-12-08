ac63927d9abcdcf1ab8b26901dc2bb47d80b8f73
3ug3ear <116048121+3ug3ear@users.noreply.github.com>
2022-12-07 19:11:53 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1. Write a console.log() statement that answers the following question: What worked
+// well for you over the last 9 weeks that aided in your learning process?
+
+console.log("What worked well for you over the last 9 weeks that aided in your learning process?");
+console.log("To better understand the lessons introduced in Javascript, I found spaced out repetition was key in understanding how to actually utilize this code properly.");
+
+// 2. Write a console.log() statement that answers the following question: What would you
+// change about the course?
+
+console.log("What would you change about the course?");
+console.log("Through frustration and worry that this course can produce in a new student i would suggest more varying repetitions of applying code to better grasp it");
+
+// 3. Write a console.log() statement that answers the following question: Did you learn
+// what you expected to learn in this course?
+
+console.log("Did you learn what you expected to learn in this course?");
+console.log("I suppose i had no idea what to expect before this course, but although i feel a little overwhelmed by some of the broader concepts I do feel i learned many of the basic structures of Javascript");
+
+// 4. Write a console.log() statement that answers the following question: What concepts
+// were new to you and what concepts had you encountered already in the past?
+
+console.log("What concepts were new to you and what concepts had you encountered already in the past?");
+console.log("All the concepts were completely new to me before this class.");
